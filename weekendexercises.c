c1e83b1179008262ea7d0d76911d645db37fafd0
Yiğit Doğan <yigitdogan2k@gmail.com>
2023-11-18 17:02:25 +0300

    wknd

---
+++ weekendexercises.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/*int main (void){
+    int num1;
+    int num2;
+    int decideIt;
+    printf("enter enter your first number: ");
+    scanf("%d", &num1);
+    printf("enter enter your second number: ");
+    scanf("%d", &num2);
+    printf("sum-1, substract-2, multiply-3, divide-4\nChoose one of them: ");
+    scanf("%d", &decideIt);
+    if (decideIt==1){
+        printf("result is: %d", num1+num2);
+    } else if (decideIt==2){
+        printf("result is: %d", num1-num2);
+    } else if (decideIt==3){
+        printf("result is: %d", num1*num2);
+    } else {
+        printf("result is: %d", num1/num2);
+    }
+    return 0;
+}*/
+
+/*int main(void){
+    int num1;
+    int num2;
+    int i;
+    float rem1;
+    int rem2;
+    scanf("%f", &num1);
+    float remainderFinder;
+    for(i=0; i<=num1; i++){
+        float rem1=num1%i;
+        if (rem1=0)
+        {
+            printf("divisors of num1: %d", &rem1);
+        }
+    }
+    return 0;
+}*/
