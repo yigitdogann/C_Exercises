3a922afffde287836783c6b315f2db01b033253d
Yiğit Doğan <yigitdogan2k@gmail.com>
2023-12-07 13:01:39 +0300

    Create drawing-a-tree.c

---
+++ drawing-a-tree.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main (void){
+    printf("---DRAWING A TREE---\n");
+    
+    int rows = 0;
+    printf("I want a tree with height of: ");
+    scanf("%d", &rows);
+    
+    for(int j = 1; j<=rows; j++){//i is current row
+    
+        for(int i = 1; i<=(rows-j); i++){
+            printf(" ");
+        }
+        
+        for(int i = 1; i<=(2*j-1); i++){
+            printf("*");
+        }
+        printf("\n");
+        
+    }
+    return 0;
+}
