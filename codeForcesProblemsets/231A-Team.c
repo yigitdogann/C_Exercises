cd89560e3a03d771c73c2056c04c486a774969db
Yiğit Doğan <yigitdogan2k@gmail.com>
2023-12-10 17:54:07 +0300

    Update 231A-Team.c

--- codeForcesProblemsets/231A-Team.c
+++ codeForcesProblemsets/231A-Team.c
@@ -3,15 +3,16 @@
 int main(void){
     int n=0, res=0;
     scanf("%d", &n);
-    
+    int p=0, v=0, t=0;
+
     for (int i = 1; i <= n; i++){
-        int p=0, v=0, t=0;
         scanf("%d %d %d", &p, &v, &t);
+        
         if((p+v+t)>=2){
-            res++; printf("%d", res);
-        }
+            res=res+1;
+        }else{}
         
     }
-    
+    printf("%d", res);
     return 0;
 }
