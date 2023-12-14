62c60ed788f5bdce9f432b0dbbc136ff5d47fc39
Yiğit Doğan <yigitdogan2k@gmail.com>
2023-12-14 12:57:21 +0300

    Create 266A-Stones-on-the-Table.c

---
+++ codeForcesProblemsets/266A-Stones-on-the-Table.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int n=0;
+    scanf("%d", &n);//n=number of stones
+    char colors[n];
+
+    for(int i=1; i<=n; i++){
+        scanf("%s", colors);
+    }//n adet renk sayısı al R-ed G-reen B-lue
+}
