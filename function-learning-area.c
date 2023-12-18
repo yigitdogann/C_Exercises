74dddeb06149e31a48a3fc7dd04f00ee9dbfbee1
Yiğit Doğan <yigitdogan2k@gmail.com>
2023-12-18 11:59:33 +0300

    Create function-learning-area.c

---
+++ function-learning-area.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+void myFunction(){
+        printf("\n\tThis is my first ever function in C and im so happy about it.\n\n");
+    }
+
+int main (void){
+    printf("---FUNCTION_LEARNING_PAGE---\n");
+    
+    myFunction();
+    return 0;
+}
