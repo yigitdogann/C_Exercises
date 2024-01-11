345c73157c89127f335929ee1cb4356e65851d92
Yiğit Doğan <yigitdogan2k@gmail.com>
2024-01-11 17:42:28 +0300

    Create common-divisors-function.c

---
+++ exercisesForFinals/common-divisors-function.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int divisor(int num1, int num2);
+
+int main(void){
+    printf("--COMMON_DIVISORS--\n");
+    
+    int num1=0, num2=0;
+    printf("NUM1: "); scanf("%d", &num1);
+    printf("NUM2: "); scanf("%d", &num2);
+    
+    divisor(num1, num2);
+    return 0;
+}
+
+int divisor(int num1, int num2){
+    int min=0;
+    if(num1<num2){
+        min=num1;
+    }else{
+        min=num2;
+    }
+    
+    printf("numbers are: ");
+    for(int i=1; i<=min; i++){
+        if(min%i==0){
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
