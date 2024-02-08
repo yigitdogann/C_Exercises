4c34562f10e51b639453a20200af599056a7df8e
Yiğit Doğan <yigitdogan2k@gmail.com>
2024-02-08 23:16:32 +0300

    Create tic-tac-toe.c

---
+++ tic-tac-toe.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int check();
+
+char square[10]={'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
+
+int main(void){
+    printf("\tTIC TAC TOE GAME\n");
+    
+    
+    return 0;
+}
+
+int check(){
+    
+    
+}
+
+//d1- tryna figure the algorithm out.
