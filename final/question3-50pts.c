c2566177afbb7432ba4fb5c7d39be8efb5665070
Yiğit Doğan <yigitdogan2k@gmail.com>
2024-02-08 23:20:52 +0300

    Create question3-50pts.c

---
+++ final/question3-50pts.c
@@ -0,0 +1,29 @@
+/*
+createrand() function creates just one random number between 100-999 and returns it.
+createarray() this function calls createrand function, gets numbers and assign to the array.
+check() function controls the polindromic number and prints it.
+array must be created in main function, don't use global variables.
+*/
+//WRITE THE CODE BELOW.
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int createrand();
+void createarray(int num);
+void check(int num);
+
+int main (void){
+    srand(time(NULL));
+    return 0;
+}
+
+int createrand(){
+    
+}
+void createarray(int num){
+    
+}
+void check(int num){
+    
+}
